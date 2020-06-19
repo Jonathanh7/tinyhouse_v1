bfdd2d7f83598ee07c20a57ca2398333dec3fed8
Jonathanh7 <Jonathanh949@gmail.com>
2020-06-19 16:38:53 -0400

    added Routes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloProvider } from 'react-apollo';
-import { Listings } from './sections'
+import { Home, Host, Listing, Listings, NotFound, User } from './sections'
 import "./styles/index.css";
 import * as serviceWorker from './serviceWorker';
 
@@ -10,9 +11,24 @@ const client = new ApolloClient({
   uri: "/api"
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listing/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  )
+}
+
 render(
   <ApolloProvider client={client}>
-    <Listings title="TinyHouse Listings" />
+   <App />
     </ApolloProvider>,
   document.getElementById('root')
 );
